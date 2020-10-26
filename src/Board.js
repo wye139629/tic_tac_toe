1055b2e66aae70a26c70a6468995ddbb2e4be98c
will <wye139629@gmail.com>
2020-10-26 12:05:29 +0800

    change component name and apply board css

---
+++ src/Board.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import "./Board.css"
+
+export default class Board extends Component {
+  render() {
+    return (
+      <div className = "board">
+
+      </div>
+    )
+  }
+}
--- src/Board.test.js
+++ src/Board.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Board from './Board';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Board />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
