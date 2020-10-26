67dad9cd57b6c402b985f4058875973c699964f3
will <wye139629@gmail.com>
2020-10-26 12:14:54 +0800

    create square component

--- src/Board.js
+++ src/Board.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 import "./Board.css"
 
+
+class Square extends Component{
+  render(){
+    return(
+      <div className="square"></div>
+    )
+  }
+}
+
+
 export default class Board extends Component {
+  constructor(props){
+    super(props)
+  }
   render() {
+
+
+
     return (
       <div className = "board">
-
+        <Square/>
       </div>
     )
   }
