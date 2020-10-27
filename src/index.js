74d686b18000a066598cd0d31ac09545c4eeaef7
will <wye139629@gmail.com>
2020-10-27 13:56:27 +0800

    create function component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Board from './Board';
 import reportWebVitals from './reportWebVitals';
+import Game from "./tic_tac_toe"
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,7 +11,12 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+ReactDOM.render(
+  <React.StrictMode>
+    <Game />
+  </React.StrictMode>,
+  document.getElementById("doc-example")
+)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
---
+++ src/tic_tac_toe.js
@@ -0,0 +1,5 @@
+import React, { Fragment, useState, useEffect } from 'react';
+
+export default function Game(){
+
+}
